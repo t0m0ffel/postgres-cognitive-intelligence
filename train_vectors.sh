939ce9b5eb95f48c1dd98e45060e66801b43028b
hendrik <hendrik.cziommer@gmx.de>
2018-02-21 18:32:11 +0100

    new tokenizer

--- train_vectors.sh
+++ train_vectors.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./fasttext skipgram -input ./corpus/actor_movie_corpus_2.txt -output ./models/actor_movie_model_full_50 -dim 300 -maxn 0 -minCount 1 -epoch 50
-./fasttext skipgram -input ./corpus/actor_movie_corpus_2.txt -output ./models/actor_movie_model_full_20 -dim 300 -maxn 0 -minCount 1 -epoch 20
+./fasttext cbow -input ./corpus/actor_movie_corpus_3.txt -output ./models/actor_movie_model_cbow -dim 300 -ws 1000 -minCount 1 -epoch 5
+./fasttext skipgram -input ./corpus/actor_movie_corpus_3.txt -output ./models/actor_movie_model_skipgram -dim 300 -ws 1000 -minCount 1 -epoch 5
 #./fasttext sent2vec -input ./corpus/actor_movie_corpus_reduced.txt -output ./models/actor_movie_model_s2v -dim 300 -maxn 0 -minCount 1 -epoch 10
 

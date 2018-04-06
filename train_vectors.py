38991eaf7969e841281eb122c857fb07f6541b54
Hendrik <hendrik.cziommer@gmx.de>
2018-04-06 15:58:21 +0200

    query similar movies by keywords

--- train_vectors.py
+++ train_vectors.py
@@ -1,8 +1,5 @@
-from gensim.models.wrappers import FastText
+import subprocess
 
 
 def train(*args):
-    for corpus in args:
-        model = FastText.train('./fasttext', corpus_file='./corpus/' + corpus,
-                               iter=20, window=3, size=300, min_count=1)
-        model.save('./models/model_' + corpus)
+    subprocess.call(['./train_vectors.sh', *args])
--- train_vectors.sh
+++ train_vectors.sh
@@ -3,7 +3,7 @@ for var in "$@"
 do
     echo "$var"
    # ./fasttext cbow -input ./corpus/${var} -output ./models/"$var"_model_cbow -dim 300 -ws 30 -minCount 1 -epoch 100 -maxn 0
-    ./fasttext skipgram -input ./corpus/${var} -output ./models/"$var"_model_skipgram -dim 300 -ws 3 -minCount 1 -epoch 20
+    ./fasttext skipgram -input ./corpus/${var} -output ./models/"$var"_model_skipgram -dim 300 -ws 3 -minCount 1 -epoch 20 -pretrainedVectors ./pretrained_vectors/wiki.en.bin
 done
 
 
